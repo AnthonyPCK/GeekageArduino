f74bde3a0947a78ad126f51371261bc82cf9635a
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-09-08 23:09:01 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,8 +31,8 @@ df = load_data(st.secrets["public_gsheets_url"])
 df.Date = pd.to_datetime(df.Date)
 df.Time = df.Date.to_timestamp
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.Time}")
+#for row in df.itertuples():
+#    st.write(f"{row.Time}")
 
 
 
@@ -48,3 +48,6 @@ st.plotly_chart(fig1, use_container_width=True)
 
 fig2 = px.scatter(df, x=df.Date, y=df.columns,range_y=[-20, 100],title="Tout :")
 st.plotly_chart(fig2, use_container_width=True)  
+
+fig3 = px.scatter(df, x=df.Date, y=df.columns,title="Tout :")
+st.plotly_chart(fig3, use_container_width=True)  
